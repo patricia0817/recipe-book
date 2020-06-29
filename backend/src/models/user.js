61bbedc19ecc910497a420226ba0d4c6239896fd
amihut <amihut@EN610723>
2020-06-29 15:51:20 +0300

    hash the password before saving

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,9 +1,9 @@
 const mongoose = require( 'mongoose' )
 const validator = require( 'validator' )
-// const bcrypt = require( 'bcryptjs' )
-// const jwt = require( 'jsonwebtoken' )
+const bcrypt = require( 'bcryptjs' )
+const jwt = require( 'jsonwebtoken' )
 
-const User = mongoose.model( 'User', {
+const userSchema = new mongoose.Schema( {
   name: {
     type: String,
     trim: true,
@@ -32,5 +32,17 @@ const User = mongoose.model( 'User', {
     }
   }
 } )
+
+userSchema.pre( 'save', async function( next ) {
+  const user = this
+
+  if ( user.isModified( 'password' ) ) {
+    user.password = await bcrypt.hash( user.password, 8 )
+  }
+
+  next()
+} )
+
+const User = mongoose.model( 'User', userSchema )
 
 module.exports = User
--- backend/src/routers/user.js
+++ backend/src/routers/user.js
@@ -2,34 +2,40 @@ const express = require( 'express' )
 const router = new express.Router()
 const User = require( '../models/user' )
 
-router.post( '/users', ( req, res ) => {
+router.post( '/users', async ( req, res ) => {
   const user = new User( req.body )
 
-  user.save().then( () => {
+  try {
+    await user.save()
     res.status( 201 ).send( user )
-  } ).catch( ( e ) => {
+  } catch ( e ) {
     res.status( 400 ).send( e )
-  } )
+  }
 } )
 
-router.get( '/users', ( req, res ) => {
-  User.find( {} ).then( ( users ) => {
+router.get( '/users', async ( req, res ) => {
+  try {
+    const users = await User.find( {} )
     res.send( users )
-  } ).catch( ( e ) => {
+  } catch ( e ) {
     res.status( 500 ).send()
-  } )
+  }
 } )
 
-router.get( '/users/:id', ( req, res ) => {
-  User.findById( req.params.id ).then( ( user ) => {
+router.get( '/users/:id', async ( req, res ) => {
+  const _id = req.params.id
+
+  try {
+    const user = await User.findById( _id )
+
     if ( !user ) {
       return res.status( 404 ).send()
     }
 
     res.send( user )
-  } ).catch( ( e ) => {
-    res.status( 500 ).send( e )
-  } )
+  } catch ( e ) {
+    res.status( 500 ).send()
+  }
 } )
 
 router.patch( '/users/:id', async ( req, res ) => {
@@ -42,7 +48,11 @@ router.patch( '/users/:id', async ( req, res ) => {
   }
 
   try {
-    const user = await User.findByIdAndUpdate( req.params.id, req.body, { new: true, runValidators: true } )
+    const user = await User.findById( req.params.id )
+
+    updates.forEach( ( update ) => user[ update ] = req.body[ update ] )
+
+    await user.save()
 
     if ( !user ) {
       return res.status( 404 ).send()
