87d82aa0c4c266fcda456a6845fec54d96c17f6e
amihut <amihut@EN610723>
2020-06-30 12:03:40 +0300

    generating authentication tokens

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -30,8 +30,24 @@ const userSchema = new mongoose.Schema( {
         throw new Error( 'Password cannot contain "password"' )
       }
     }
-  }
+  },
+  tokens: [ {
+    token: {
+      type: String,
+      required: true
+    }
+  } ]
 } )
+
+userSchema.methods.generateAuthToken = async function() {
+  const user = this
+  const token = jwt.sign( { _id: user._id.toString() }, 'secretKey' )
+
+  user.tokens = user.tokens.concat( { token } )
+  await user.save()
+
+  return token
+}
 
 userSchema.statics.findByCredentials = async ( email, password ) => {
   const user = await User.findOne( { email } )
