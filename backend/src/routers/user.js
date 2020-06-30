ba7a541b2a0eea1788777370ba8c46f87e7d16fd
amihut <amihut@EN610723>
2020-06-30 15:10:12 +0300

    accepting authentication tokens

--- backend/src/routers/user.js
+++ backend/src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require( 'express' )
 const router = new express.Router()
 const User = require( '../models/user' )
+const auth = require( '../middleware/auth' )
 
 router.post( '/users', async ( req, res ) => {
   const user = new User( req.body )
@@ -24,7 +25,7 @@ router.post( '/users/login', async ( req, res ) => {
   }
 } )
 
-router.get( '/users', async ( req, res ) => {
+router.get( '/users', auth, async ( req, res ) => {
   try {
     const users = await User.find( {} )
     res.send( users )
