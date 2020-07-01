557d43aca0b396d1117a475b867b0731ab152ce4
amihut <amihut@EN610723>
2020-07-01 13:48:46 +0300

    add sign in functionality

--- frontend/src/components/SignInForm.js
+++ frontend/src/components/SignInForm.js
@@ -1,23 +1,41 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
+import Axios from 'axios'
 
 function SignInForm() {
+  const [ username, setUsername ] = useState()
+  const [ email, setEmail ] = useState()
+  const [ password, setPassword ] = useState()
+
+  async function handleSubmit( e ) {
+    e.preventDefault()
+    try {
+      await Axios.post( 'http://localhost:3000/register', { username, email, password } )
+      setUsername( '' )
+      setEmail( '' )
+      setPassword( '' )
+      console.log( 'User was succesfully created.' )
+    } catch ( e ) {
+      console.log( 'There was an error.' );
+    }
+  }
+
   return (
-    <Form className="signin-form col-xs-12 col-lg-6 offset-lg-3">
+    <Form onSubmit={ e => handleSubmit( e ) } className="signin-form col-xs-12 col-lg-6 offset-lg-3">
       <Form.Group controlId='formRetypeBasicPassword'>
         <Form.Label>Name</Form.Label>
-        <Form.Control type='text' placeholder='Enter name' />
+        <Form.Control onChange={ e => setUsername( e.target.value ) } type='text' placeholder='Enter name' value={ username } />
       </Form.Group>
       <Form.Group controlId='formBasicEmail'>
         <Form.Label>Email address</Form.Label>
-        <Form.Control type='email' placeholder='Enter email' />
+        <Form.Control onChange={ e => setEmail( e.target.value ) } type='email' placeholder='Enter email' value={ email } />
         <Form.Text className='text-muted'>
           We'll never share your email with anyone else.
       </Form.Text>
       </Form.Group>
       <Form.Group controlId='formBasicPassword'>
         <Form.Label>Password</Form.Label>
-        <Form.Control type='password' placeholder='Password' />
+        <Form.Control onChange={ e => setPassword( e.target.value ) } type='password' placeholder='Password' value={ password } />
       </Form.Group>
       <Button variant='outline-dark' type='submit'>
         Submit
